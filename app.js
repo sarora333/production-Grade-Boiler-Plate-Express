d05950223ca547ac627f898b699454227a172412
Shivam Arora <shivamarora0109@gmail.com>
2026-05-03 14:17:55 +0530

    feat: add info file for better understanding

--- app.js
+++ app.js
@@ -60,7 +60,6 @@ app.get("/", (req, res) => {
 // 6. 404 Handler for undefined routes
 app.use((req, res, next) => {
   res.status(404).render("404");
- 
 });
 
 // 7. Global Error Handler (MUST be last)
